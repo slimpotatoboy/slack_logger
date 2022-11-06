bece662160eed85844efa871c933b4908cbb15ae
slimpotatoboy <edwinmhzn@gmail.com>
2022-11-06 22:19:40 +0545

    Initial Release

--- lib/slack_logger.dart
+++ lib/slack_logger.dart
@@ -1,7 +1,29 @@
 library slack_logger;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class SlackLogger {
+  static Future send(String webhookUrl, String message) async {
+    try {
+      var postBody = jsonEncode({"text": message});
+      Uri url = Uri.parse(webhookUrl);
+      final response = await http.post(
+        url,
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        },
+        body: postBody,
+      );
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return e.toString();
+    }
+  }
 }
--- test/slack_logger_test.dart
+++ test/slack_logger_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:slack_logger/slack_logger.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  // });
 }
