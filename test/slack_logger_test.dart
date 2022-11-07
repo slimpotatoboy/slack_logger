f4254289ea5c7d99e1cf323644e9821523cea689
Ashim Upadhaya <justayyme@gmail.com>
2022-11-07 10:49:40 +0545

    chore: :wastebasket: Code clean up

--- test/slack_logger_test.dart
+++ test/slack_logger_test.dart
@@ -1,7 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:slack_logger/slack_logger.dart';
-
 void main() {
   // test('adds one to input values', () {
   //   final calculator = Calculator();
