c201d3bcdfbd2f66fba6ac33cc9241c05e429d91
Ashim Upadhaya <justayyme@gmail.com>
2022-11-07 10:44:31 +0545

    refactor: :recycle: Make [SlackLogger] singleton

--- lib/slack_logger.dart
+++ lib/slack_logger.dart
@@ -4,10 +4,30 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class SlackLogger {
-  static Future send(String webhookUrl, String message) async {
+  static SlackLogger? _instance;
+  static SlackLogger get instance {
+    _assertInstance();
+    return _instance!;
+  }
+
+  factory SlackLogger({
+    required String webhookUrl,
+  }) {
+    _instance = SlackLogger._internal(webhookUrl);
+
+    return _instance!;
+  }
+
+  SlackLogger._internal(this.webhookUrl);
+
+  final String webhookUrl;
+
+  Future send(String message) async {
+    _assertInstance();
+
     try {
       var postBody = jsonEncode({"text": message});
-      Uri url = Uri.parse(webhookUrl);
+      Uri url = Uri.parse(_instance!.webhookUrl);
       final response = await http.post(
         url,
         headers: {
@@ -25,5 +45,12 @@ class SlackLogger {
     } catch (e) {
       return e.toString();
     }
+  }
+
+  static void _assertInstance() {
+    assert(
+      _instance != null,
+      "[SlackLogger] instance needs to be created first.",
+    );
   }
 }
