5b0d07a4574f90301c6f02aa38856374d37eb8c7
slimpotatoboy <edwinmhzn@gmail.com>
2023-03-19 23:14:33 +0545

    Example updated

--- example/example.dart
+++ example/example.dart
@@ -85,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             Image.network(
                 "https://dipenmaharjan.com.np/assets/png/Thumbnail.png"),
